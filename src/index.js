555e920239a304ec60431f54f5631a9d3731b5b2
Bohdan Driha <ddrigass@gmail.com>
2025-03-29 23:27:36 +0200

    feat: add contents

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
+import { initCursorCanvas } from "./scripts/cursorCanvas";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+initCursorCanvas();
